9ad1a21468d888eb4b1b6ddd25b9dd762b81736e
nu-jey <yp50777@naver.com>
2023-05-30 22:45:12 +0900

    - CoreMotion: 가속도 측정 - 정확한 값 측정을 위한 LowPassFilter 구현

--- WOWATACH Watch App/ContentView.swift
+++ WOWATACH Watch App/ContentView.swift
@@ -6,21 +6,45 @@
 //
 
 import SwiftUI
-
+import CoreMotion
 struct ContentView: View {
+    let lamda: Double = 2 * Double.pi * 5 * 0.002
+    
+    var motionManager = CMMotionManager()
+    @State private var accX : Double = 0
+    @State private var accY : Double = 0
+    @State private var accZ : Double = 0
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text(wowa.bodyPart.joined())
+            Text("\(accX)")
+            Text("\(accY)")
+            Text("\(accZ)")
             Button("버튼") {
                 print("button pressed")
             }
         }
         .padding()
+        .onAppear {
+            if motionManager.isAccelerometerAvailable {
+                motionManager.accelerometerUpdateInterval = 1.0 / 60.0
+                motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+                    guard let data = data, error == nil else {
+                        return
+                    }
+                    print(data.acceleration.x, data.acceleration.y, data.acceleration.z)
+                    accX = lowPassFilter(rawAcc: data.acceleration.x, preAcc: accX)
+                    accY = lowPassFilter(rawAcc: data.acceleration.x, preAcc: accY)
+                    accZ = lowPassFilter(rawAcc: data.acceleration.x, preAcc: accZ)
+                }
+            }
+        }
     }
-    
+    func lowPassFilter(rawAcc: Double, preAcc: Double) -> Double {
+        return lamda / (1 + lamda) * rawAcc + 1 / (1 + lamda) * preAcc
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
